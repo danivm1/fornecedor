38ce087377ce06fd8e2988baad9a13b65191c119
danivm1 <48190557+danivm1@users.noreply.github.com>
2019-11-29 02:32:08 -0300

    Add files via upload

--- src/components/Novo.js
+++ src/components/Novo.js
@@ -11,55 +11,64 @@ class Novo extends Component {
                     <div className="novoTitulo">
                         <label align="center">Novo cadastro</label>
                     </div>
-                    <div className="novoDivNome">
-                        <label className="novoLblNome">Nome: </label>
-                        <input type="textField" className="novoTxtNome" placeholder="Nome"/>
-                    </div>
-                    <div className="novoDivEmail">
-                        <label className="novoLblEmail">Email: </label>
-                        <input type="textField" className="novoTxtEmail" placeholder="exemplo@email.com"/>
-                    </div>
-                    <div className="novoDivEndereco">
-                        <label className="novoLblCidade">Cidade: </label>
-                        <input type="textField" className="novoTxtCidade" placeholder="cidade"/>
-                        <label className="novoLblUf">UF: </label>
-                        <select className="novoSelUf" required>
-                            <option></option>
-                            <option>AC</option>
-                            <option>AL</option>
-                            <option>AM</option>
-                            <option>AP</option>
-                            <option>BA</option>
-                            <option>CE</option>
-                            <option>DF</option>
-                            <option>ES</option>
-                            <option>GO</option>
-                            <option>MA</option>
-                            <option>MG</option>
-                            <option>MS</option>
-                            <option>MT</option>
-                            <option>PA</option>
-                            <option>PE</option>
-                            <option>PR</option>
-                            <option>RJ</option>
-                            <option>RN</option>
-                            <option>RO</option>
-                            <option>RR</option>
-                            <option>RS</option>
-                            <option>SC</option>
-                            <option>SP</option>
-                            <option>PA</option>
-                            <option>PE</option>
-                            <option>TO</option>
-                        </select>
-                        <div className="novoDivTelefone">
-                            <label className="novoLblTelefone">Telefone: </label>
-                            <input type="textField" className="novoTxtTelefone" placeholder="Ex: (14)998121879"/>
-                        </div>
-                        <div className="novoDivBtnCad">
-                            <label>asdgh</label>
-                        </div>
-                    </div>
+                    <table>
+                        <tr className="novoDivNome">
+                            <td id="tdEsq"><label className="novoLblNome">Nome: </label></td>
+                            <td id="tdDir"><input type="textField" className="novoTxtNome" placeholder="Nome"/></td>
+                        </tr>
+                        <tr className="novoDivEmail">
+                            <td id="tdEsq"><label className="novoLblEmail">Email: </label></td>
+                            <td id="tdDir"><input type="textField" className="novoTxtEmail" placeholder="exemplo@email.com"/></td>
+                        </tr>
+                        <tr className="novoDivEndereco">
+                            {/* <tr className="novoDivCidade"> */}
+                                <td className="novoTdLblCidade"><label className="novoLblCidade">Cidade: </label></td>
+                                <td className="novoTdTxtCidade"><input type="textField" className="novoTxtCidade" placeholder="cidade"/></td>
+                            {/* </tr> */}
+                            {/* <tr className="novoDivUf"> */}
+                            <td className="novoTdLblUf"><label className="novoLblUf">UF: </label></td>
+                            <td className="novoTdSelUf"><select className="novoSelUf" required>
+                                <option></option>
+                                <option>AC</option>
+                                <option>AL</option>
+                                <option>AM</option>
+                                <option>AP</option>
+                                <option>BA</option>
+                                <option>CE</option>
+                                <option>DF</option>
+                                <option>ES</option>
+                                <option>GO</option>
+                                <option>MA</option>
+                                <option>MG</option>
+                                <option>MS</option>
+                                <option>MT</option>
+                                <option>PA</option>
+                                <option>PE</option>
+                                <option>PR</option>
+                                <option>RJ</option>
+                                <option>RN</option>
+                                <option>RO</option>
+                                <option>RR</option>
+                                <option>RS</option>
+                                <option>SC</option>
+                                <option>SP</option>
+                                <option>PA</option>
+                                <option>PE</option>
+                                <option>TO</option>
+                            </select></td>
+                            {/* </tr> */}
+                        </tr>
+                        <tr className="novoDivTelefone">
+                            <td id="tdEsq"><label className="novoLblTelefone">Telefone: </label></td>
+                            <td id="tdDir">
+                                <input type="textField" className="novoTxtTelefone" placeholder="Ex: (14)998121879"/>
+                                <div className="novoDivBtnCad wrap">
+                                    <label>Cadastrar</label>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                    
                 </form>
             </div>
         )
